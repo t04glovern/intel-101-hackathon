255ca039850a40c64371c8cd952aedb8f0fe3ce1
Nathan Glover <nathan@glovers.id.au>
2017-07-21 23:57:36 +0800

    Updated formatting

--- rpi/ble-server.py
+++ rpi/ble-server.py
@@ -1,6 +1,5 @@
 from bluepy import btle
 import time
-import binascii
 import struct
 
 print "Connecting..."
@@ -10,7 +9,7 @@ dev = btle.Peripheral("98:4F:EE:0D:02:84")
 print "Services..."
 # Prints all the services available for the Arduino 101
 for svc in dev.services:
-	print str(svc)
+    print str(svc)
 
 # Arduino 101 imuService UUID
 arduinoDevice = btle.UUID("917649A0-D98E-11E5-9EEC-0002A5D5C51B")
@@ -42,14 +41,14 @@ union
 arduinoAccValue = arduinoService.getCharacteristics(imuAccUUID)[0]
 arduinoGyroValue = arduinoService.getCharacteristics(imuGyroUUID)[0]
 
-while(1):
-	time.sleep(1.0)
-	accVal = arduinoAccValue.read()
-	gyroVal = arduinoGyroValue.read()
+while (1):
+    time.sleep(1.0)
+    accVal = arduinoAccValue.read()
+    gyroVal = arduinoGyroValue.read()
 
-	# Unpack the float arrays
-	accVal_unpacked = struct.unpack('3f',accVal)
-	gyroVal_unpacked = struct.unpack('3f',gyroVal)
-	
-	print accVal_unpacked
-	print gyroVal_unpacked
+    # Unpack the float arrays
+    accVal_unpacked = struct.unpack('3f', accVal)
+    gyroVal_unpacked = struct.unpack('3f', gyroVal)
+
+    print accVal_unpacked
+    print gyroVal_unpacked
