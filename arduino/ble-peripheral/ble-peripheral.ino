920cfa97a2aa5cf9e2cb9d467ba4bc10e3f0f135
Nathan Glover <nathan@glovers.id.au>
2017-07-21 19:08:00 +0000

    added code for both peripherals and centrals

---
+++ arduino/ble-peripheral/ble-peripheral.ino
@@ -0,0 +1,221 @@
+/**
+   Include Libraries
+*/
+#include <CurieBLE.h>
+#include <CurieTime.h>
+
+
+/**
+   BLE Peripheral Initialization
+   - this is the board we are using -
+*/
+BLEPeripheral blePeripheral;
+
+
+/**
+   Define BLE Service
+   - this is the service that hosts our arduino characteristics -
+*/
+BLEService arduinoSensorService("2571bb9c-3516-4399-94d4-7b531e97100c");
+
+
+/**
+   Define BLE Characteristics
+   - Referenced by our central device (RPi) and polled for data
+
+   Info: https://www.arduino.cc/en/Reference/BLECharacteristicConstructor
+   Usage:
+    > UUID
+    > Properties (BLERead, BLENotify, BLEWrite)
+    > Max Byte Length (Max allowed 20 bytes)
+*/
+BLECharacteristic identifier("8e3034a6-e53a-41f6-8d38-ac5f4ce185db", BLERead , 15);
+BLECharacteristic timestamp("b43521f5-16ec-4936-849c-6c218e7bdab5", BLERead | BLENotify | BLEWrite, 20);
+BLECharacteristic data_point_1("beb203a6-bdc3-4111-9569-b2a3aa121f00", BLERead, 5);
+BLECharacteristic data_point_2("0d41699d-cae3-41d4-8504-86fd0072b31a", BLERead , 5 );
+BLECharacteristic data_point_3("8cd25b99-1690-47bc-b3ac-de594fb313de", BLERead, 5);
+BLECharacteristic data_point_4("0b86e58a-1456-410c-a69b-601aaeaa2a46", BLERead, 5);
+
+
+/**
+   Define Pins for our data points
+   - Placeholders for actual sensor attachments
+*/
+int data_pin_1 = 1;
+int data_pin_2 = 2;
+int data_pin_3 = 3;
+int data_pin_4 = 4;
+
+/**
+   Define out systems name
+   - We will use this field as our identifier characteristic and local name
+*/
+String dev_id = "arduino-node-01";
+
+
+void setup() {
+
+  // Initialze serial port for debugging communications
+  Serial.begin(9600);
+  Serial.println("Starting " + dev_id + " communications");
+
+  // Set data pin modes
+  pinMode(data_pin_1, INPUT);
+  pinMode(data_pin_2, INPUT);
+  pinMode(data_pin_3, INPUT);
+  pinMode(data_pin_4, INPUT);
+
+  // BLE Setup
+  blePeripheral.setLocalName("arduino-node-01");
+  blePeripheral.setAdvertisedServiceUuid(arduinoSensorService.uuid());
+
+  // BLE Service
+  blePeripheral.addAttribute(arduinoSensorService);
+
+  // BLE Characteristics for this service
+  blePeripheral.addAttribute(identifier);
+  blePeripheral.addAttribute(timestamp);
+  blePeripheral.addAttribute(data_point_1);
+  blePeripheral.addAttribute(data_point_2);
+  blePeripheral.addAttribute(data_point_3);
+  blePeripheral.addAttribute(data_point_4);
+
+  // Initial Sensor Get
+  readSensors();
+
+  // BLE Peripherals go live
+  blePeripheral.begin();
+
+}
+
+void loop() {
+
+  // Checks if any central devices try to connect
+  BLECentral central = blePeripheral.central();
+  
+  if (central) {
+    
+    // When connected, prints central's MAC address
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+
+    while (central.connected()) {
+      
+      // While connected to central, we read our sensors
+      readSensors();
+      
+    }
+
+    // When central disconnects, print status
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void readSensors() {
+
+  // Get Identifier
+  getIdentifier();
+
+  // Set/Get DateTime
+  getTimestamp();
+
+  // Get Data 1
+  getDataPoint1();
+
+  // Get Data 2
+  getDataPoint2();
+
+  // Get Data 3
+  getDataPoint3();
+
+  // Get Data 4
+  getDataPoint4();
+
+  return;
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+
+void getIdentifier() {
+  char charArray[15];
+  dev_id.toCharArray(charArray, 50);
+  identifier.setValue(charArray);
+}
+
+void getTimestamp() {
+  int datetime = now();
+  char charArray[20];
+  dtostrf(datetime, 20, 0, charArray);
+
+  timestamp.setValue(charArray);
+
+  Serial.print("Ok, Time = ");
+  print2digits(hour());
+  Serial.write(':');
+  print2digits(minute());
+  Serial.write(':');
+  print2digits(second());
+  Serial.print(", Date (D/M/Y) = ");
+  Serial.print(day());
+  Serial.write('/');
+  Serial.print(month());
+  Serial.write('/');
+  Serial.print(year());
+  Serial.println();
+}
+
+void getDataPoint1() {
+  int sensorValue = digitalRead(data_pin_1);
+  
+  Serial.print("DataPoint1 is: ");
+  Serial.println(sensorValue);
+
+  char charArray[5];
+  dtostrf(sensorValue, 5, 0, charArray);
+  
+  data_point_1.setValue(charArray);
+}
+
+void getDataPoint2() {
+  int sensorValue = digitalRead(data_pin_2);
+  
+  Serial.print("DataPoint2 is: ");
+  Serial.println(sensorValue);
+
+  char charArray[5];
+  dtostrf(sensorValue, 5, 0, charArray);
+  
+  data_point_2.setValue(charArray);
+}
+
+void getDataPoint3() {
+  int sensorValue = digitalRead(data_pin_3);
+  
+  Serial.print("DataPoint3 is: ");
+  Serial.println(sensorValue);
+
+  char charArray[5];
+  dtostrf(sensorValue, 5, 0, charArray);
+  
+  data_point_3.setValue(charArray);
+}
+
+void getDataPoint4() {
+  int sensorValue = digitalRead(data_pin_4);
+  
+  Serial.print("DataPoint4 is: ");
+  Serial.println(sensorValue);
+
+  char charArray[5];
+  dtostrf(sensorValue, 5, 0, charArray);
+  
+  data_point_4.setValue(charArray);
+}
+
+
---
+++ rpi/ble-get-reading.py
@@ -0,0 +1,144 @@
+from bluepy.btle import Scanner, DefaultDelegate, UUID, Peripheral
+import time
+import binascii
+
+# Bluetooth LE Settings
+deviceName = "rpi-central-01"
+
+# Defines how often to poll for device if data is found/not found
+sleepIfFound = 60  # secs
+sleepIfNotFound = 10  # secs
+
+# Define Bluetooth LE UUIDs
+validUUIDs = [[], [], [], [], [], []]
+
+validUUIDs[0].append("8e3034a6-e53a-41f6-8d38-ac5f4ce185db")
+validUUIDs[0].append("identifier")
+
+validUUIDs[1].append("b43521f5-16ec-4936-849c-6c218e7bdab5")
+validUUIDs[1].append("timestamp")
+
+validUUIDs[2].append("beb203a6-bdc3-4111-9569-b2a3aa121f00")
+validUUIDs[2].append("data_point_1")
+
+validUUIDs[3].append("0d41699d-cae3-41d4-8504-86fd0072b31a")
+validUUIDs[3].append("data_point_2")
+
+validUUIDs[4].append("8cd25b99-1690-47bc-b3ac-de594fb313de")
+validUUIDs[4].append("data_point_3")
+
+validUUIDs[5].append("0b86e58a-1456-410c-a69b-601aaeaa2a46")
+validUUIDs[5].append("data_point_4")
+
+uuidCount = len(validUUIDs)
+
+
+# Write data to a date store
+def add_sensor_data(identifier, timestamp, datePoint1, datePoint2, datePoint3, datePoint4):
+    return 'ok'
+
+
+# Function to determine BTLE devices available
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print "Discovered device", dev.addr
+        elif isNewData:
+            print "Received new data from", dev.addr
+
+
+class MyDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        print("A notification was received: %s" % data)
+
+        val = binascii.b2a_hex(data)
+        val = binascii.unhexlify(val)
+        val = str(val).strip()
+        val = int(val)
+        getScrapDataFromSensor(val)
+        print "Amount in bin : " + str(val)
+
+
+def connect_to_device(mac_address):
+    # Get a reference to the connect device peripheral
+    arduino_peripheral = Peripheral(mac_address)
+
+    # Iterate over all the known UUIDs
+    for x in range(0, uuidCount):
+        uuid_val = validUUIDs[x][0]
+        try:
+            arduino_characteristics = arduino_periph.getCharacteristics(uuid=uuid_val)[0]
+
+            # If the characteristic has the BLERead property
+            if arduino_characteristics.supportsRead():
+                val = binascii.b2a_hex(ch.read())
+                val = binascii.unhexlify(val)
+                val = str(val).strip()
+                val = int(val)
+                print validUUIDs[x][1] + ": " + str(val)
+
+                if validUUIDs[x][1] == "identifier":
+                    identifier = val
+                elif validUUIDs[x][1] == "timestamp":
+                    timestamp = val
+                elif validUUIDs[x][1] == "data_point_1":
+                    data_point_1 = val
+                elif validUUIDs[x][1] == "data_point_2":
+                    data_point_2 = val
+                elif validUUIDs[x][1] == "data_point_3":
+                    data_point_3 = val
+                elif validUUIDs[x][1] == "data_point_4":
+                    data_point_4 = val
+        finally:
+            print "Done"
+
+    # Add sensor data to the datastore
+    add_sensor_data(identifier, timestamp, data_point_1, data_point_2, data_point_3, data_point_4)
+
+    # Disconnect from the device peripheral
+    arduino_peripheral.disconnect()
+    return overallMsg
+
+
+def get_mac(val_name):
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(4.0)
+
+    mac_addr = "empty"
+
+    for dev in devices:
+        for (adtype, desc, value) in dev.getScanData():
+            if desc == "Complete Local Name" and value == val_name:
+                print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+                print "  %s = %s" % (desc, value)
+                mac_addr = dev.addr
+    return mac_addr
+
+
+# Used to ensure we only check every 30 seconds
+check_time = 0
+
+while 1:
+
+    while check_time < time.time():
+
+        # Check if arduino node is available
+        arduino_mac = get_mac("arduino-node-01")
+
+        if (arduino_mac != "empty"):
+            try:
+                print connect_to_device(arduino_mac)
+                check_time = time.time() + sleepIfFound
+                arduino_mac = "empty"
+                print "Reading obtained, waiting %s seconds".format(sleepIfFound)
+            except:
+                arduino_mac = "empty"
+        else:
+            print "No device found, searching again in %s seconds".format(sleepIfNotFound)
+            check_time = time.time() + sleepIfNotFound
